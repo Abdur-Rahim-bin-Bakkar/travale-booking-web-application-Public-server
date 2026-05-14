027c6669726c5176ea610f0f41792563a79233e5
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-05-14 08:25:50 +0600

    add bookon post system

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config()
 const express = require('express')
 const app = express()
 const cors = require("cors")
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
@@ -24,19 +24,69 @@ const client = new MongoClient(uri, {
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+let dbCollection = null;
+let bookingCollection = null;
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const db = client.db('wanderlust')
+        dbCollection = db.collection('destinations')
+        bookingCollection = db.collection('bookings')
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+    } catch (error) {
+        console.error(error);
     }
 }
-run().catch(console.dir);
+run()
+app.get('/destination', async(req, res) => {
+    const data = await dbCollection.find().toArray()
+    // console.log(data)
+
+    res.send(data)
+    // console.log('get hocche')
+
+})
+app.get('/destination/:id', async(req, res) => {
+    const id = req.params.id;
+    const data = await dbCollection.findOne({_id:new ObjectId(id)})
+    // console.log(data)
+
+    res.send(data)
+    // console.log('get hocche')
+
+})
+app.patch('/destination/:id',async(req,res)=>{
+    const id = req.params.id;
+    console.log(id)
+    const result = await dbCollection.updateOne( {_id: new ObjectId(id)},{$set:req.body})
+    res.send(result)
+    // console.log('hit hocche')
+})
+app.delete('/destination/:id',async(req,res)=>{
+    const id = req.params.id;
+    const result = await dbCollection.deleteOne({_id:new ObjectId(id)})
+    res.send(result)
+    console.log('delete hocche')
+})
+app.post('/destination',async (req, res) => {
+    const newDestination = req.body;
+    const result = await dbCollection.insertOne(newDestination)
+    res.send(result)
+    // console.log(result, 'result hocche')
+
+})
+app.post('/booking', async(req,res)=>{
+    const newBooking = req.body;
+    const result = await bookingCollection.insertOne(newBooking)
+    res.send(result)
+})
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
